2d0de84df698a91fb43794cb0a9cfdd0c40bcdba
Yuki.N <Syukkic@gmail.com>
2025-03-31 22:17:20 +0800

    lang: zh-Hant

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,11 +1,11 @@
 import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component }: PageProps) {
   return (
-    <html>
+    <html lang="zh-Hant">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>YGO Auxiliary Tool</title>
+        <title>遊戲王輔助工具</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
